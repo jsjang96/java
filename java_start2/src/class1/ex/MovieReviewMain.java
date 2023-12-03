60937bfdf7fbe1b5a5ce92e0e9234a3d9fc0e5e4
jinsu <jsjang96@gmail.com>
2023-12-03 15:06:46 +0900

    class example 생성 및 코드짜기 => 배열 부분 공부하기.

---
+++ java_start2/src/class1/ex/MovieReviewMain.java
@@ -0,0 +1,25 @@
+package class1.ex;
+
+public class MovieReviewMain {
+
+	public static void main(String[] args) {
+		MovieReview[] reviews = new MovieReview[2];
+		
+		MovieReview movieReview1 = new MovieReview();
+		movieReview1.title = "인셉션";
+		movieReview1.review = "인생은 무한루프";
+		reviews[0] = movieReview1;
+		
+		MovieReview movieReview2 = new MovieReview();
+		movieReview2.title = "어바웃 타임";
+		movieReview2.review = "인생 시간 영화";
+		reviews[1] = movieReview2;
+		
+		
+		for(int i=0; i<reviews.length;i++) {
+			System.out.println("영화 제목: " + reviews[i].title + " , 리뷰: "+ reviews[i].review);
+		
+		}
+ 	}
+
+}
